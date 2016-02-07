f3685149969f614d1d7597808e0e5c3c8dd16196
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2016-02-07 15:16:17 +0000

    Refactor TubeStation tests, add extra invalid cases

--- src/test/scala/io/github/howyp/TubeStationSpec.scala
+++ src/test/scala/io/github/howyp/TubeStationSpec.scala
@@ -3,23 +3,28 @@ package io.github.howyp
 import org.scalatest.matchers.Matcher
 import org.scalatest.{FreeSpec, Matchers}
 
-import scala.io.Source
 import scala.util.parsing.combinator.Parsers
 
 class TubeStationSpec extends FreeSpec with Matchers {
   "Tube Stations" - {
-    "should be able to be parsed from a CSV line" - {
+    "can be parsed from a CSV line" - {
       "given a valid example then it should extract name and location" in {
-        val csvLine = "\"Blackhorse Road\",51.585777,-0.039626"
-        TubeStation.parse(TubeStation.station, csvLine) should parseTo (TubeStation("Blackhorse Road",Location(51.585777,-0.039626)))
+        TubeStation.parse(TubeStation.station, "\"Blackhorse Road\",51.585777,-0.039626") should parseTo (
+          TubeStation("Blackhorse Road",Location(51.585777,-0.039626))
+        )
       }
-      "given a invalid line then it should not parse" in {
-        val csvLine = "\"Blackhorse Road,51.585777,-0.039626"
-        TubeStation.parse(TubeStation.station, csvLine) should failParsing
+      "given a line with a missing quote then it should not parse" in {
+        TubeStation.parse(TubeStation.station, "\"Blackhorse Road,51.585777,-0.039626") should failParsing
+      }
+      "given a line with a missing field then it should not parse" in {
+        TubeStation.parse(TubeStation.station, "\"Blackhorse Road\",-0.039626") should failParsing
+      }
+      "given a line with an incorrectly typed field then it should not parse" in {
+        TubeStation.parse(TubeStation.station, "\"Blackhorse Road\",test,-0.039626") should failParsing
       }
     }
     "should be loadable as a set from a file" - {
-      "given a sample CSV string then it should return a list of TubeStations" in {
+      "given a sample CSV string then it should return a list of tube stations" in {
         val csvLines =
           """|"Beckton Park (DLR)",51.509994,0.055534
              |"Becontree",51.539585,0.12688
